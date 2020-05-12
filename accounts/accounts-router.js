75cef7c3c026e706a49160bda390f8fc8ac1a60e
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-12 08:55:11 -0700

    update an account by id

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -42,4 +42,20 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedPost = await db("accounts")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (updatedPost) {
+      res.status(201).json(updatedPost);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
